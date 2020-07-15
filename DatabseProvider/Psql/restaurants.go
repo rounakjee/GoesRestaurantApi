2db5cefc8ba256ef31fecd07119224c55fa848b0
Rounak Chatterjee <40384952+rounakjee@users.noreply.github.com>
2020-07-15 13:29:10 +0530

    Preloading added

--- DatabseProvider/Psql/restaurants.go
+++ DatabseProvider/Psql/restaurants.go
@@ -2,6 +2,11 @@ package Psql
 
 import "GoResturantAPI/models"
 
-func (psql *PSqlDB)CreateRestaurant(restaurant *models.Restaurant)error{
+func (psql *PSqlDB) CreateRestaurant(restaurant *models.Restaurant) error {
 	return psql.Create(restaurant).Error
+}
+func (psql *PSqlDB) AllRestaurants() []models.Restaurant {
+	var restaurants []models.Restaurant
+	psql.Preload("User").Find(&restaurants)
+	return restaurants
 }
